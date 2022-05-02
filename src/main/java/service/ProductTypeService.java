eb0868f755abccd95a8d3a5de2f9452a586ce4a1
xiaojia <895601287@qq.com>
2022-05-02 13:01:34 +0800

    Initial commit

---
+++ src/main/java/service/ProductTypeService.java
@@ -0,0 +1,14 @@
+package service;
+
+
+
+
+import pojo.ProductType;
+
+import java.util.List;
+
+public interface ProductTypeService {
+   //完成查询全部商品类型的功能
+    public List<ProductType> getAllType();
+
+}
